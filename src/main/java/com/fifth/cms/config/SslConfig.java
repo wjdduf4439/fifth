decdbe41c9a40ae1eef3cacaabf848752e987d3f
wjdduf4439 <wjdduf4439@gmail.com>
2025-01-22 03:01:56 +0900

    20250122 commit

---
+++ src/main/java/com/fifth/cms/config/SslConfig.java
@@ -0,0 +1,25 @@
+package com.fifth.cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SslConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .requiresChannel(channel -> channel
+                .anyRequest().requiresSecure()
+            );
+        return http.build();
+    }
+	
+}

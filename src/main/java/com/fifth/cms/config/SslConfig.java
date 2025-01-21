b322f91e85e30bd8ff37eadff2491d7644bd25b9
wjdduf4439 <wjdduf4439@gmail.com>
2025-01-22 03:16:15 +0900

    20250122 commit-3

--- src/main/java/com/fifth/cms/config/SslConfig.java
+++ src/main/java/com/fifth/cms/config/SslConfig.java
@@ -1,11 +1,15 @@
 package com.fifth.cms.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
-public class SslConfig {
+public class SslConfig implements WebMvcConfigurer {
 	
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "https://www.ljycms.com:80");
+    }
 	
 }
